c42800e96ccb469132cd623ed2eef53e98e66a51
TekyUser <toantekyservice@gmail.com>
2024-10-13 15:25:48 +0700

    Add files via upload

--- Project/Project/urls.py
+++ Project/Project/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import handler404
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('App.urls')),
     path('blog/', include('Blog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'App.views.error'
